c32f23ed3ed20fe272b5e33a0996f511c97b2bd2
elmerjanmark <elmer.janmark@gmail.com>
2025-05-31 10:07:45 +0200

    lkjs

--- Sensorfusion smartphone/tu_qw_no.m
+++ Sensorfusion smartphone/tu_qw_no.m
@@ -1,10 +1,10 @@
-function [x, P] = tu_qw_no(x, P, Rw)
+function [x, P] = tu_qw_no(x, P, T, Rw)
     F = eye(4);
-    G = 0;
+    G = (T/2)*Sq(x);
 
     x_ = F*x;
     P_ = F*P*F' + G * Rw * G';
-
+    
     [x, P] = mu_normalizeQ(x_, P_);
 end
 
